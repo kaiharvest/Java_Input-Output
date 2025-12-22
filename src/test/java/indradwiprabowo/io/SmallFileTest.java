0775a676bf38897f51d32279d2c514e060af6d7d
kaiharvest <indradwiprabowo27@gmail.com>
2025-12-23 05:27:01 +0700

    Write Small File

---
+++ src/test/java/indradwiprabowo/io/SmallFileTest.java
@@ -0,0 +1,22 @@
+package indradwiprabowo.io;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class SmallFileTest {
+
+    @Test
+    void writeSmallFile() throws IOException {
+        Path path1 = Path.of("small1.txt");
+        byte[] bytes = "Hello World".getBytes();
+        Files.write(path1, bytes);
+
+        Path path2 = Path.of("small2.txt");
+        Files.writeString(path2,"Hello World");
+        Assertions.assertTrue(Files.exists(path2));
+    }
+}
