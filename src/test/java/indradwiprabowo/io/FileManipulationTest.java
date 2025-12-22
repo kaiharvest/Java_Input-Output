6a3754073c8e60b18cab52064ef9caed0e567716
kaiharvest <indradwiprabowo27@gmail.com>
2025-12-23 05:00:18 +0700

    Manipulation File

---
+++ src/test/java/indradwiprabowo/io/FileManipulationTest.java
@@ -0,0 +1,23 @@
+package indradwiprabowo.io;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileManipulationTest {
+
+    @Test
+    void fileManipulation() throws IOException {
+        Path path = Path.of("file.txt");
+        Files.createFile(path);
+        Assertions.assertTrue(Files.exists(path));
+
+        Files.delete(path);
+        Assertions.assertFalse(Files.exists(path));
+    }
+
+}
