57877ec76e1f00d7dc83dab04a2dd1c2ae5fc201
kaiharvest <indradwiprabowo27@gmail.com>
2025-12-23 05:14:30 +0700

    Closable Interface

---
+++ src/test/java/indradwiprabowo/io/ClosableTest.java
@@ -0,0 +1,45 @@
+package indradwiprabowo.io;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ClosableTest {
+
+    @Test
+    void closeIO() throws IOException {
+        Path path = Path.of("pom.xml");
+        InputStream inputStream = null;
+
+        try {
+            inputStream = Files.newInputStream(path);
+            // lakukan apapun dengan input stream
+        } catch (IOException exception) {
+            Assertions.fail(exception);
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException exception) {
+                    Assertions.fail(exception);
+                }
+            }
+        }
+    }
+
+    @Test
+    void closeIOWithResource() throws IOException {
+        Path path = Path.of("pom.xml");
+
+        try (InputStream inputStream = Files.newInputStream(path)) {
+            // lakukan apapun dengan input stream
+        } catch (IOException exception) {
+            Assertions.fail(exception);
+        }
+    }
+
+}
