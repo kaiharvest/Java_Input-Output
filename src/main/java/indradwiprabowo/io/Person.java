1aaa8e13598305741191f895f8c034d6f315e7c4
kaiharvest <indradwiprabowo27@gmail.com>
2026-01-06 12:56:55 +0700

    Memory Stream

--- src/main/java/indradwiprabowo/io/Person.java
+++ src/main/java/indradwiprabowo/io/Person.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class Person implements Serializable {
 
-    public static final long serialVersionID = 1L;
+    private static final long serialVersionID = 1L;
 
     private String id;
 
